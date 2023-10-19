c3f96d2aac7e92d7f6de528b56f3e83e7e3f5280
ajcm <ajcm2020@icloud.com>
2023-10-19 09:30:22 +0200

    add first projects

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# spring-examples
+# Spring Examples
 Spring and Spring Boot project examples
+
+
+
+#### springboot3-helloword
+run spring boot application and list beans
+
+
+#### springboot3-rest-helloword
+spring boot with rest just returning helloword
---
+++ springboot3-rest-helloword/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,34 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path="/api")
+public class GreetingController {
+
+	Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+
+	@GetMapping("/")
+	public String hello() {
+		logger.warn("test..");
+
+		return "Hello";
+	}
+}
