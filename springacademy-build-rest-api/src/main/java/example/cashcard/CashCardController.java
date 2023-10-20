81848ddfb2f9998e6dac577c6b44dc762588ba5e
Armando Jorge Marques <ajcm@users.noreply.github.com>
2023-10-20 15:58:56 +0200

    adding repository

--- springacademy-build-rest-api/src/main/java/example/cashcard/CashCardController.java
+++ springacademy-build-rest-api/src/main/java/example/cashcard/CashCardController.java
@@ -1,26 +1,24 @@
 package example.cashcard;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+
+    Logger LOG = LoggerFactory.getLogger(CashCardController.class);
     private CashCardRepository cashCardRepository;
 
 
     public CashCardController(CashCardRepository cashCardRepository) {
         this.cashCardRepository = cashCardRepository;
-    }
-
-    @GetMapping
-    public ResponseEntity<String> getRoot() {
-        return ResponseEntity.ok("hello");
     }
 
     @GetMapping("/{requestedId}")
@@ -31,6 +29,21 @@ public class CashCardController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard cashCard, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(cashCard);
+
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+
+        LOG.info("createCashCard::new card: {} ",savedCashCard.id());
+
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 
 }
