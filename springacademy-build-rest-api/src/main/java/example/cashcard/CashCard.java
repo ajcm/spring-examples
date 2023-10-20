d96fea00cc3370a9f9f6bec70b0cc996010d924d
Armando Jorge Marques <ajcm@users.noreply.github.com>
2023-10-20 15:27:21 +0200

    adding repository

--- springacademy-build-rest-api/src/main/java/example/cashcard/CashCard.java
+++ springacademy-build-rest-api/src/main/java/example/cashcard/CashCard.java
@@ -1,6 +1,8 @@
 package example.cashcard;
 
-public record CashCard(Long id, Double amount) {
+import org.springframework.data.annotation.Id;
+
+public record CashCard(@Id  Long id, Double amount) {
 
 }
 
