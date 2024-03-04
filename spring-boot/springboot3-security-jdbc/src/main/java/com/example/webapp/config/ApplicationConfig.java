b04db10455a67d7ef12994f65241f7543f05cb70
Armando Jorge Marques <ajcm@users.noreply.github.com>
2024-03-04 19:40:28 +0100

    new projects

---
+++ spring-boot/springboot3-security-jdbc/src/main/java/com/example/webapp/config/ApplicationConfig.java
@@ -0,0 +1,34 @@
+package com.example.webapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ApplicationConfig {
+
+//    @Autowired
+//    private DataSource dataSource;
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth)
+//            throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .withDefaultSchema()
+//                .withUser(User.withUsername("user")
+//                        .password(passwordEncoder().encode("pass"))
+//                        .roles("USER"));
+//    }
+}
