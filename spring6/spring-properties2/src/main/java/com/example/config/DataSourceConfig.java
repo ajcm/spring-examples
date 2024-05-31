56c355fe9cac46631b53e4d9d9fc384bc92c6719
Armando Jorge Marques <ajcm@users.noreply.github.com>
2024-05-31 15:15:14 +0200

    Update DataSourceConfig.java

--- spring6/spring-properties2/src/main/java/com/example/config/DataSourceConfig.java
+++ spring6/spring-properties2/src/main/java/com/example/config/DataSourceConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 public class DataSourceConfig {
 
     @Bean("databaseName")
+    @Lazy
     public String getName(@Value("${database.name}") String dbname) {
         return dbname;
     }
