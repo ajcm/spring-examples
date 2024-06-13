4b2ab247a1fb608a15c2ac25b759578bd5c6d692
Armando Jorge Marques <ajcm@users.noreply.github.com>
2024-06-13 16:52:47 +0200

    Update JdbcApplicationTest.java

--- spring6/spring-jdbc2/src/test/java/com/example/JdbcApplicationTest.java
+++ spring6/spring-jdbc2/src/test/java/com/example/JdbcApplicationTest.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +9,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
@@ -17,28 +19,37 @@ public class JdbcApplicationTest {
 
     @BeforeEach
     public void setUp(@Autowired JdbcTemplate template) {
-
         template.update("insert into users values (?,?,?)", "007", "James Bond", "some@email");
-
         template.update("insert into users values (?,?,?)", UUID.randomUUID().toString(), "mark", "some@email");
-
         template.update("insert into users values (?,?,?)", UUID.randomUUID().toString(), "mark", "some@email");
-
         template.update("insert into users values (?,?,?)", UUID.randomUUID().toString(), "mark", "some@email");
-
-
         template.update("insert into users values (?,?,?)", UUID.randomUUID().toString(), "mark", "some@email");
-
     }
 
     @Test
     public void test(@Autowired JdbcTemplate template) {
 
-     var list =   template.queryForList("select * from users");
+        //retrieves a list
+        List<Map<String, Object>> list = template.queryForList("select * from users");
+        Assertions.assertEquals(list.size(), 5);
 
-     list.size();
+        //only gets one row
+        Map<String, Object> map = template.queryForMap("select * from users where id like '007'");
+        Assertions.assertEquals(map.get("Name"), "James Bond");
 
+        Long count = template.queryForObject("select count(*) from users", Long.class);
+        Assertions.assertEquals(count, 5);
     }
 
+
+    @Test
+    public void test2(@Autowired JdbcTemplate template) {
+
+        //retrieves a list
+        List<Map<String, Object>> list = template.query()
+
+
+
+    }
 
 }
