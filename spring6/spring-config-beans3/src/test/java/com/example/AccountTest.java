41805dfa3e3e1746a2b6a92c3305527376fbbac1
Armando Jorge Marques <ajcm@users.noreply.github.com>
2024-06-18 10:47:16 +0200

    Update AccountTest.java

--- spring6/spring-config-beans3/src/test/java/com/example/AccountTest.java
+++ spring6/spring-config-beans3/src/test/java/com/example/AccountTest.java
@@ -4,15 +4,14 @@ import com.example.accounts.AccountConfiguration;
 import com.example.accounts.AccountService;
 import com.example.accounts.TransferService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = AccountConfiguration.class)
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = AccountConfiguration.class)
+@SpringJUnitConfig(classes = AccountConfiguration.class)
 public class AccountTest {
 
     @Autowired
